776f07af6154aa30afd77101675be8d4652f25a4
Vladislav.Chesnokov <vlad2012chesnokov@mail.ru>
2022-08-01 11:33:44 +0500

    Initial commit

---
+++ bot.py
@@ -0,0 +1,87 @@
+import logging
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.dispatcher import FSMContext
+from service import update_ozon_items, get_ozon_items, get_sima_land_items
+
+BOT_TOKEN = '5485325873:AAHyJaeUdm8IimgHCOeZ9ulj0s97LTO1saA'
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class BotState(StatesGroup):
+    api_key = State()
+    client_id = State()
+    sima_token = State()
+
+
+@dp.message_handler(commands=['start'], state='*')
+async def send_welcome(message: types.Message):
+    await bot.send_message(message.from_user.id, 'Введите ключ API от озона')
+    await BotState.next()
+
+
+@dp.message_handler(state=BotState.api_key)
+async def get_apikey(message: types.Message, state: FSMContext):
+    if len(message.text) != 36:
+        await bot.send_message(message.from_user.id, 'Неверно введен ключ! Введите еще раз')
+        return
+    else:
+        async with state.proxy() as data:
+            data['API_KEY'] = message.text
+        await bot.send_message(message.from_user.id, 'Введите Client ID')
+        await BotState.next()
+
+
+@dp.message_handler(state=BotState.client_id)
+async def get_clientid(message: types.Message, state: FSMContext):
+    if len(message.text) != 6:
+        await bot.send_message(message.from_user.id, 'Неверно введен Client-ID ! Введите еще раз')
+        return
+    else:
+        async with state.proxy() as data:
+            data['CLIENT_ID'] = str(message.text)
+        await bot.send_message(message.from_user.id, 'Введите SIMA LAND JWT')
+        await BotState.next()
+
+
+@dp.message_handler(state=BotState.sima_token)
+async def get_sima_token(message: types.Message, state: FSMContext):
+    if 'Bearer' not in message.text:
+        await bot.send_message(message.from_user.id, 'Неверно введен токен ! Копируйте вместе с Bearer')
+        return
+    else:
+        async with state.proxy() as data:
+            data['SIMA_LAND_TOKEN'] = str(message.text)
+        await bot.send_message(message.from_user.id, 'Начинаю работу...')
+        update_ozon_items(
+            get_sima_land_items(get_ozon_items(data['API_KEY'], data['CLIENT_ID']), data['SIMA_LAND_TOKEN']),
+            data['API_KEY'], data['CLIENT_ID'])
+        await state.finish()
+
+
+# todo здесь сделать остановку работы бота
+@dp.message_handler(state='*', commands='stop')
+async def stop_state(message: types.Message, state: FSMContext):
+    cur_state = await state.get_state()
+    if not cur_state:
+        return
+    await state.finish()
+    await bot.send_message(message.from_user.id, 'Вышел из состояния')
+
+
+def register_handlers(dp: Dispatcher):
+    dp.register_message_handler(send_welcome, commands='start')
+    dp.register_message_handler(get_apikey)
+    dp.register_message_handler(get_clientid)
+    dp.register_message_handler(get_sima_token)
+    dp.register_message_handler(stop_state)
+
+
+register_handlers(dp)
+
+executor.start_polling(dp, skip_updates=True)
---
+++ service.py
@@ -0,0 +1,79 @@
+import requests
+
+# OZON init
+API_KEY = "dc4a490e-949a-4a11-b5d4-1e8381ab9602"
+CLIENT_ID = "402856"
+
+# SIMA LAND init
+SIMA_LAND_MIN = 2
+
+
+# SIMA_LAND_TOKEN = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NTk3MTg1NDksImlhdCI6MTY1OTExMzc0OSwianRpIjoyNzA3MjQxLCJuYmYiOjE2NTkxMTM3NDl9.g7JlVOzc4QB3Tv5mBoVo8AN0kjygCfrGmRdqhWRiHsM '
+#
+
+def get_ozon_items(API_KEY, CLIENT_ID):
+    ozon_products_ids = []
+    res = requests.post('https://api-seller.ozon.ru/v3/product/info/stocks',
+                        headers={'Api-Key': API_KEY, 'Client-Id': CLIENT_ID},
+                        json={
+                            "filter": {
+                                "visibility": "VISIBLE"
+                            },
+                            "last_id": "",
+                            "limit": 1000
+                        })
+    for i in res.json().get('result')['items']:
+        ozon_products_ids.append(int(i.get('offer_id')))
+    return ozon_products_ids
+
+
+def get_sima_land_items(ozon_products_ids, SIMA_LAND_TOKEN):
+    sima_land = []
+    for i in ozon_products_ids:
+        print(f'sima {i}')
+        try:
+            response = requests.get(
+                f'https://www.sima-land.ru/api/v5/item/{i}?view=brief&by_sid=true',
+                headers={
+                    'accept': 'application/json',
+                    'X-Api-Key': SIMA_LAND_TOKEN,
+                    'Authorization': SIMA_LAND_TOKEN,
+                },
+
+                params={
+                    'view': 'brief',
+                    'by_sid': 'true',
+                }
+            )
+            if int(response.json()['balance']) < SIMA_LAND_MIN:
+                sima_land.append({'sid': response.json()['sid'], 'stock': 0})
+            else:
+                sima_land.append({'sid': response.json()['sid'], 'stock': response.json()['balance']})
+        except Exception as e:
+            print(e)
+    return sima_land
+
+
+def update_ozon_items(sima_land, API_KEY, CLIENT_ID):
+    response = requests.post('https://api-seller.ozon.ru/v1/warehouse/list',
+                             headers={'Api-Key': API_KEY, 'Client-Id': CLIENT_ID})
+    warehouse_id = response.json()['result'][0].get('warehouse_id')
+    stocks = []
+    if sima_land:
+        for i in sima_land:
+            print(f'ozon {i}')
+            stocks.append({"offer_id": str(i['sid']), "stock": i['stock'], "warehouse_id": warehouse_id})
+            # todo сделать запросы через внутреннее апи if len(stocks) % 50 == 0 or len(sima_land) < 50:
+            res = requests.post('https://api-seller.ozon.ru/v2/products/stocks',
+                                headers={'Api-Key': API_KEY, 'Client-Id': CLIENT_ID},
+                                json={
+                                    "stocks": stocks
+                                })
+            for item in res.json()['result']:
+                if not item['updated']:
+                    print("Товар не обновлен")
+                stocks = []
+    else:
+        return 'Товары не требуют обновления или слишком низкий SIMA_MIN'
+
+# update_ozon_items(get_sima_land_items(get_ozon_items()))
